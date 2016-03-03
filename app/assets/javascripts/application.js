f276c74f914428a6cbc37ceb37ec87b4f8eb8afb
Arturo Dlrt <arc.ofyours@gmail.com>
2016-03-03 11:01:25 -0600

    Fix for js not loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,30 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap
 //= require cocoon
-//= require_tree .
-
-$(function(){
-  $('#zoo_image').change(function(){
-    previewImage(this);
-    $('#preview').show();
-  })
-});
-
-var previewImage = function(input){
-  if(input.files && input.files[0]){
-    var reader = new FileReader();
-
-    reader.onload = function(e){
-      $('#preview').attr('src', e.target.result)
-    };
-
-    reader.readAsDataURL(input.files[0])
-  }
-};
-
-var showCurrentImage = function(url){
-  $('#preview').attr('src', url).show()
-};
