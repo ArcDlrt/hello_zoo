ac8d15be34810056654496746451b875b891a1f9
Arturo Dlrt <arc.ofyours@gmail.com>
2016-03-03 06:37:17 -0600

    CRUD for breed

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -2,6 +2,7 @@ class BreedsController < ApplicationController
 
   def new
     @breed = Breed.new
+    respond_with @breed
   end
 
   def create
@@ -15,11 +16,20 @@ class BreedsController < ApplicationController
   end
 
   def edit
-
+    @breed = Breed.find(params[:id])
+    respond_with @breed
   end
 
   def update
+    @breed = Breed.find(params[:id])
+    @breed.update_attributes breed_params
+    respond_with @breed, location: breeds_path
+  end
 
+  def destroy
+    @breed = Breed.find(params[:id])
+    @breed.destroy
+    respond_with @breed
   end
 
   private
