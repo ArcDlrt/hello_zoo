96b218b731404b93dc9920602450b9c62a93a654
Arturo Dlrt <arc.ofyours@gmail.com>
2016-03-03 05:08:24 -0600

    CRUD for zoo

--- app/controllers/zoos_controller.rb
+++ app/controllers/zoos_controller.rb
@@ -1,7 +1,42 @@
 class ZoosController < ApplicationController
 
+  def new
+    @zoo = Zoo.new
+    respond_with @zoo
+  end
+
+  def create
+    @zoo = Zoo.create zoo_params
+    respond_with @zoo, location: zoos_path
+  end
+
   def index
     @zoos = Zoo.all
+    respond_with @zoos
+  end
+
+
+  def edit
+    @zoo = Zoo.find(params[:id])
+    respond_with @zoo
+  end
+
+  def update
+    @zoo = Zoo.find(params[:id])
+    @zoo.update_attributes zoo_params
+    respond_with @zoo, location: zoos_path
+  end
+
+  def destroy
+    @zoo = Zoo.find(params[:id])
+    @zoo.destroy
+    respond_with @zoo
+  end
+
+  private
+
+  def zoo_params
+    params.require(:zoo).permit(:name, :description)
   end
 
 end
