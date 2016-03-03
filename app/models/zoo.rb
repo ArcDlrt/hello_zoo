7b49aeabddd3690d21ed883bd11a2ac67c818779
Arturo Dlrt <arc.ofyours@gmail.com>
2016-03-03 05:30:33 -0600

    Added animal model

--- app/models/zoo.rb
+++ app/models/zoo.rb
@@ -1,5 +1,7 @@
 class Zoo < ActiveRecord::Base
 
+  has_many :animals
+
   validates :name, :description, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303084205) do
+ActiveRecord::Schema.define(version: 20160303111053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "animals", force: :cascade do |t|
+    t.string   "name",        limit: 150
+    t.string   "description"
+    t.integer  "zoo_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "animals", ["zoo_id"], name: "index_animals_on_zoo_id", using: :btree
 
   create_table "zoos", force: :cascade do |t|
     t.string   "name",        limit: 100
